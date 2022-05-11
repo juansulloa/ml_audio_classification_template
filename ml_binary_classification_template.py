d8942c0c44d0c180993a885e109c5ab3eba93771
GabrielPerilla <gperilla@humboldt.org.co>
2022-05-11 13:04:40 -0500

    update
    impor pyplot
    save csv with all params
    save best model
    load best model

--- ml_binary_classification_template.py
+++ ml_binary_classification_template.py
@@ -14,16 +14,18 @@ import os
 from maad import sound
 from librosa import feature
 from sklearn.model_selection import train_test_split, StratifiedKFold, GridSearchCV
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import f1_score
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import f1_score, classification_report
+from joblib import dump, load
+import matplotlib.pyplot as plt
 
 #%% Set variables
-path_annotations = 'audio_labels.csv'  # manual annotations in csv table
-path_audio = 'audio/'  # directory where the audio data is located
+path_annotations = r"audio_labels.csv"  # manual annotations in csv table
+path_audio = r"audio"  # directory where the audio data is located
 target_fs = 10000  # set target sampling rate for audio
 
 #%% Load annotations
-df = pd.read_csv(path_annotations)  
+df = pd.read_csv(path_annotations, sep=";")
 
 #%% Compute features
 df_features = pd.DataFrame()
@@ -31,10 +33,12 @@ df_features = pd.DataFrame()
 for idx_row, row in df.iterrows():
     full_path_audio = os.path.join(path_audio, row.sample_idx)
     s, fs = sound.load(full_path_audio)
+    # trim audio to first 10 seconds
+    s_trim = sound.trim(s, fs, 0, 10)
     # resample
-    s_resamp = sound.resample(s, fs, target_fs, res_type='kaiser_fast')
+    s_resamp = sound.resample(s_trim, fs, target_fs, res_type='kaiser_fast')
     # transform
-    mfcc = feature.mfcc(y=s_resamp, sr=target_fs, n_mfcc=20, n_fft=1024, 
+    mfcc = feature.mfcc(y=s_resamp, sr=target_fs, n_mfcc=20, n_fft=1024,
                         win_length=1024, hop_length=512, htk=True)
     mfcc = np.median(mfcc, axis=1)
     # format dataframe
@@ -46,22 +50,29 @@ for idx_row, row in df.iterrows():
 #%% Split development and test data
 X = df_features.loc[:,df_features.columns.str.startswith('mfcc')]
 y = (df_features.label=='LLUVIA').astype(int)
-X_train, X_test, y_train, y_test = train_test_split(X, y, 
-                                                    test_size=0.2, 
+X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                    test_size=0.2,
                                                     shuffle=True,
                                                     random_state=42)
 
 
-#%% Tune model hyperparameters
-clf = RandomForestClassifier(n_jobs=-1, class_weight='balanced_subsample')
+#%% Tune model fixed hyperparameters
+clf = MLPClassifier(random_state=245)
 
 
-# Set tuning strategy
-param_grid = {'n_estimators' : [1, 5, 10, 100, 300, 500],
-              'max_features' : [2, 6, 10, 14, 18]}
+# Set tuning strategy for unfixed parameters
+param_grid = {"hidden_layer_sizes":[2,5,10,50,100,150,200],
+            "activation": ('identity', 'logistic', 'tanh', 'relu'),
+            "alpha": [0.0001, 0.001, 0.01, 0.1],
+            "learning_rate":('constant', 'invscaling', 'adaptive'),
+            "solver":('lbfgs', 'sgd', 'adam'),
+            "learning_rate_init":[0.001, 0.01, 0.1, 1],
+            "max_iter":[200,500,1000]
+            }
 
 skf = StratifiedKFold(n_splits=10)
-clf_gs = GridSearchCV(clf, param_grid, scoring=['f1'], 
+metric = 'f1'
+clf_gs = GridSearchCV(clf, param_grid, scoring=[metric],
                            refit='f1', cv=skf, return_train_score=True,
                            n_jobs=-1, verbose=2).fit(X_train, y_train)
 
@@ -69,6 +80,7 @@ clf_gs = GridSearchCV(clf, param_grid, scoring=['f1'],
 #%% Evaluation: compute metrics, error analysis
 print('Mean cross-validated score of the best_estimator:', clf_gs.best_score_)
 print('Parameter setting that gave the best results on hold out data', clf_gs.best_params_)
+
 
 # Plots to explore results of cross-validation
 params = ['param_max_features', 'param_n_estimators']
@@ -88,7 +100,16 @@ ax[1,1].plot(clf_gs.cv_results_[params[1]].tolist(), clf_gs.cv_results_[metrics[
 ax[1,1].set_xlabel(params[1]); ax[1,1].set_ylabel(metrics[1]);
 
 
+#%% Saving parameters and scores
+results = pd.DataFrame(clf_gs.cv_results_).sort_values("rank_test_"+metric)
+results.to_csv(path_or_buf = os.path.join(path_audio, "tuning"+metric+".csv"), index=True)
+
 #%% Final evaluation on test data
 y_pred = clf_gs.predict(X_test)
 score = f1_score(y_test, y_pred)
+print(classification_report(y_test, y_pred, labels=[1,0]))
 print('Final test metrics:', score)
+
+#%% Saving best model
+dump(clf_gs.best_estimator_, os.path.join(path_audio, 'MLP_rain.joblib'))
+clf_best = load(os.path.join(path_audio, 'MLP_rain.joblib'))
